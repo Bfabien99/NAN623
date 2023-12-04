4ebd4dae2ac0811d4a38d45987e81f88a7bdee79
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2023-12-04 22:03:29 +0000

    ajout du suffix 'Controller' aux différents controllers

--- EXO1/index.php
+++ EXO1/index.php
@@ -2,17 +2,17 @@
 $uri = $_SERVER["REQUEST_URI"];
 
 switch ($uri) {
-    case "/": require("controllers/index.php");
+    case "/": require("controllers/indexController.php");
     break;
-    case "/doctors": require("controllers/doctors.php");
+    case "/doctors": require("controllers/doctorsController.php");
     break;
-    case "/services": require("controllers/services.php");
+    case "/services": require("controllers/servicesController.php");
     break;
-    case "/blog-single": require("controllers/blog-single.php");
+    case "/blog-single": require("controllers/blog-singleController.php");
     break;
-    case "/news-single": require("controllers/news-single.php");
+    case "/news-single": require("controllers/news-singleController.php");
     break;
-    case "/contact-us": require("controllers/contact.php");
+    case "/contact-us": require("controllers/contactController.php");
     break;
     default: require("views/404.php");
 }
